8fa32930390ce4248fb44d84b8e3b3cd3657cca1
sancho-sumy <www.sancho@gmail.com>
2023-05-01 13:03:18 +0300

    feat(course project): modules & optimizing

--- 00-Course-project/src/app/app.component.ts
+++ 00-Course-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { LoginService } from './loggin.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,13 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent implements OnInit {
   title = 'course-project';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private logginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.authService.autoLogin();
+    this.logginService.printLog('Hello from AppComponent ngOnInit');
   }
 }
--- 00-Course-project/src/app/shopping-list/shopping-list.component.ts
+++ 00-Course-project/src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { LoginService } from '../loggin.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -12,13 +13,17 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private igChangeSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
     this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
       (newIngredients: Ingredient[]) => (this.ingredients = newIngredients)
     );
+    this.loginService.printLog('Hello from ShoppingListCompnent ngOnInit')
   }
 
   ngOnDestroy(): void {
